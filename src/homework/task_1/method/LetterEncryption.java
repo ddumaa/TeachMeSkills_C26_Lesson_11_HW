85cb56bd95ce3a073e0f27856bfbc25a008f5b58
ddumaa <dimaduma62@gmail.com>
2023-12-07 18:56:36 +0300

    fix

--- src/homework/task_1/method/LetterEncryption.java
+++ src/homework/task_1/method/LetterEncryption.java
@@ -2,14 +2,9 @@ package homework.task_1.method;
 
 public class LetterEncryption {
     public static void letterEncryption (String numberDocuments){
-        char[] array = numberDocuments.toCharArray();
-        array[5] = '*';
-        array[6] = '*';
-        array[7] = '*';
-        array[14] = '*';
-        array[15] = '*';
-        array[16] = '*';
-        String str = new String(array);
-        System.out.println(str);
+        StringBuffer sb = new StringBuffer(numberDocuments);
+        sb.replace(5,8,"***");
+        sb.replace(14,17,"***");
+        System.out.println(sb);
     }
 }
--- src/homework/task_2/Run.java
+++ src/homework/task_2/Run.java
@@ -1,4 +1,9 @@
 package homework.task_2;
 
 public class Run {
+    public static void main(String[] args) {
+        String sentence = " Vi gleder oss til en ny leilighet i utkanten av Oslo";
+
+        System.out.println();
+    }
 }
